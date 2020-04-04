856f521a7dcb1794807519e86db6dd521523102a
PyPcDeV <52856398+PyPcDeV@users.noreply.github.com>
2020-04-05 00:57:38 +0300

    Add files via upload

--- DB_account.py
+++ DB_account.py
@@ -17,16 +17,13 @@ def UsernameExists(username):
 
 def GetAccountDataByID(acc_id):
     data.execute("SELECT * FROM Accounts WHERE acc_id = '%s'" % acc_id) # command for sql
-    try:
-        _accountData = data.fetchall()[0]  # result
-        accountData = {}
-        accountData["acc_id"] = _accountData[0]
-        accountData["acc_name"] = _accountData[1]
-        accountData["acc_username"] = _accountData[2]
-        accountData["acc_showRealName"] = _accountData[3]
-        accountData["acc_balance"] = _accountData[4]
-    except:
-        raise ValueError("unknown id: '%s'" % acc_id)
+    _accountData = data.fetchall()[0]  # result
+    accountData = {}
+    accountData["acc_id"] = _accountData[0]
+    accountData["acc_name"] = _accountData[1]
+    accountData["acc_username"] = _accountData[2]
+    accountData["acc_showRealName"] = _accountData[3]
+    accountData["acc_balance"] = _accountData[4]
     return accountData
 
 def CreateNewAccount(acc_id, acc_name): # create new account for user
@@ -37,10 +34,3 @@ def CreateNewAccount(acc_id, acc_name): # create new account for user
 def SetAccountDataElement(acc_id, element, value): # update the database elementt o value by acc_id
     data.execute("UPDATE Accounts SET %s = '%s' WHERE acc_id = %s" % (element, value, acc_id))
     database.commit()
-
-
-
-
-
-
-
--- DB_rooms.py
+++ DB_rooms.py
@@ -1,5 +1,8 @@
 from DB_connector import *
-import time
+
+def SetRoomOnline(room_id, value):
+    data.execute("UPDATE Rooms SET room_online = %s WHERE room_id = '%s'" % (value, room_id))
+    database.commit()
 
 def GetRoomsOnline():
     data.execute("SELECT room_online FROM Rooms")
@@ -8,6 +11,23 @@ def GetRoomsOnline():
     for x in range(1, 21):
         onlines[str(x)] = result[x-1][0]
     return onlines
+
+def GetRoomOnline(room_id):
+    data.execute("SELECT room_online FROM Rooms WHERE room_id = '%s'" % room_id)
+    return data.fetchall()[0][0]
+
+
+def GetRoomsTypes():
+    data.execute("SELECT room_type FROM Rooms")
+    result = data.fetchall()
+    types = {}
+    for x in range(1, 21):
+        types[str(x)] = result[x - 1][0]
+    return types
+
+def GetRoomType(room_id):
+    data.execute("SELECT room_type FROM Rooms WHERE room_id = '%s'" % room_id)
+    return data.fetchall()[0][0]
 
 def AddUserToRoom(room_id, acc_id):
     data.execute("SELECT room_usersData FROM Rooms WHERE room_id = '%s'" % room_id)
@@ -26,6 +46,11 @@ def RemoveUserFromRoom(room_id, acc_id):
     data.execute("UPDATE Rooms SET room_usersData = '%s' WHERE room_id = '%s'" % (strdata, room_id))
     database.commit()
 
+def RemoveAllUsersFromRoom(room_id):
+    data.execute("UPDATE Rooms SET room_usersData = '%s' WHERE room_id = '%s'" % ("", room_id))
+    database.commit()
+
+
 def SetUserMoneyRate(room_id, acc_id, value):
     data.execute("SELECT room_usersData FROM Rooms WHERE room_id = '%s'" % room_id)
     strdata = data.fetchall()[0][0]
@@ -34,6 +59,27 @@ def SetUserMoneyRate(room_id, acc_id, value):
     stop_index = start_index + subsrt.index(";") + 1
     strdata = strdata.replace(strdata[start_index:stop_index], "user*%s:%s;" % (acc_id, value))
     data.execute("UPDATE Rooms SET room_usersData = '%s' WHERE room_id = '%s'" % (strdata, room_id))
-
     database.commit()
 
+def GetUserMoneyRate(room_id, acc_id):
+    data.execute("SELECT room_usersData FROM Rooms WHERE room_id = '%s'" % room_id)
+    strdata = data.fetchall()[0][0]
+    start_index = strdata.index("user*%s" % acc_id) + 5 + len(str(acc_id)) + 1
+    stop_index = start_index + strdata[start_index:].index(";")
+    return strdata[start_index:stop_index]
+
+def GetAllUsersMoneyRate(room_id):
+    data.execute("SELECT room_usersData FROM Rooms WHERE room_id = '%s'" % room_id)
+    strdata = data.fetchall()[0][0]
+
+    result = {}
+
+    for x in range(int(GetRoomOnline(room_id))):
+        result[strdata[5:strdata.index(":")]] = strdata[strdata.index(":")+1:strdata.index(";")]
+        strdata = strdata[strdata.index(";")+1:]
+
+    return result
+
+def GetRoomRateOfMoney(room_id):
+    data.execute("SELECT room_rateOfMoney FROM Rooms WHERE room_id = '%s'" % room_id)
+    return data.fetchall()[0][0]

c34a89dfbe6bfe5ae2cf814f10d308b8ddde55fc
PyPcDeV <52856398+PyPcDeV@users.noreply.github.com>
2020-04-05 18:56:10 +0300

    new update

--- 2xM0NEY.py
+++ 2xM0NEY.py
@@ -12,7 +12,7 @@ admin = '@AAnimeshnikk'
 z_admin = '@pypcdev'
 chat = 'https://t.me/twoxchat'
 news = 'https://t.me/twoxnews'
-id = '560083718' # Твой ид, что-бы бот кидал тебе вce, что происходит в боте
+id = '560083718' # Твой ид, что-бы бот кидал тебе вce, что происходит в боте 123
 
 # Для киви апи
 # token = ''         # https://qiwi.com/api
